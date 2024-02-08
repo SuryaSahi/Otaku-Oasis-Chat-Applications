f06932695f6af3ea6be9e0a33e3386efef92ec69
Surya <suryasahi900@gmail.com>
2024-02-08 19:05:00 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const projectID = "79593389-2366-4a40-b672-251631a56936";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
+
+//projectID="79593389-2366-4a40-b672-251631a56936"
+//       userName="Luffy"
+//     userSecret="123123"
